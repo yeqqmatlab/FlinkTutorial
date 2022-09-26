c088579092bdb7b7b84905c7ada1365fe1237cdc
yqq <1037123792@qq.com>
2022-09-26 18:33:51 +0800

    flink state of stream

--- src/main/java/com/atguigu/apitest/state/StateDemo2_KeyedState.java
+++ src/main/java/com/atguigu/apitest/state/StateDemo2_KeyedState.java
@@ -1,13 +1,4 @@
 package com.atguigu.apitest.state;
-/**
- * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
- * <p>
- * Project: FlinkTutorial
- * Package: com.atguigu.apitest.state
- * Version: 1.0
- * <p>
- * Created by wushengran on 2020/11/10 15:49
- */
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.RichMapFunction;
@@ -18,29 +9,28 @@ import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * @ClassName: StateTest2_KeyedState
- * @Description:
- * @Author: wushengran on 2020/11/10 15:49
- * @Version: 1.0
+ * description: state KeyedState
+ * Created by yqq
+ * 2022-09-26
  */
-public class StateTest2_KeyedState {
+public class StateDemo2_KeyedState {
     public static void main(String[] args) throws Exception{
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         // socket文本流
         DataStream<String> inputStream = env.socketTextStream("ip248", 7777);
 
-        // 转换成SensorReading类型
+        // 定义一个有状态的map操作, 统计当前sensor数据个数
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
-            String[] fields = line.split(",");
-            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+            String[] split = line.split(",");
+            return new SensorReading(split[0], new Long(split[1]), new Double(split[2]));
         });
 
         // 定义一个有状态的map操作，统计当前sensor数据个数
-        SingleOutputStreamOperator<Integer> resultStream = dataStream
-                .keyBy("id")
-                .map( new MyKeyCountMapper() );
+        SingleOutputStreamOperator<Integer> resultStream = dataStream.keyBy("id")
+                .map(new MyKeyCountMapper());
 
         resultStream.print();
 
@@ -49,44 +39,46 @@ public class StateTest2_KeyedState {
 
     // 自定义RichMapFunction
     public static class MyKeyCountMapper extends RichMapFunction<SensorReading, Integer>{
+
+        //类型状态声明
         private ValueState<Integer> keyCountState;
 
-        // 其它类型状态的声明
+        //其他类型状态声明
         private ListState<String> myListState;
         private MapState<String, Double> myMapState;
         private ReducingState<SensorReading> myReducingState;
 
+
         @Override
         public void open(Configuration parameters) throws Exception {
-            keyCountState = getRuntimeContext().getState(new ValueStateDescriptor<Integer>("key-count", Integer.class, 0));
 
+            keyCountState = getRuntimeContext().getState(new ValueStateDescriptor<Integer>("key-count", Integer.class, 0));
             myListState = getRuntimeContext().getListState(new ListStateDescriptor<String>("my-list", String.class));
             myMapState = getRuntimeContext().getMapState(new MapStateDescriptor<String, Double>("my-map", String.class, Double.class));
-//            myReducingState = getRuntimeContext().getReducingState(new ReducingStateDescriptor<SensorReading>())
+//            myReducingState = getRuntimeContext().getReducingState(new ReducingStateDescriptor<SensorReading>(SensorReading));
+
         }
 
         @Override
         public Integer map(SensorReading value) throws Exception {
             // 其它状态API调用
-            // list state
-            for(String str: myListState.get()){
-                System.out.println(str);
+            for (String str : myListState.get()) {
+                System.out.println("str = " + str);
             }
 //            myListState.add("hello");
-//            // map state
-//            myMapState.put("1",35.9);
+//
+//            myMapState.put("1", 35.9);
 //            myMapState.put("2", 12.3);
 //            myMapState.remove("2");
 
             Double aDouble = myMapState.get("1");
             System.out.println("aDouble = " + aDouble);
-            // reducing state
-//            myReducingState.add(value);
 
-            myMapState.clear();
+//            myMapState.clear();
 
             Integer count = keyCountState.value();
             count++;
+
             keyCountState.update(count);
             return count;
         }
